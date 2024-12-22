20f7ec1e00d6ce8c8de35361c9f8abbd93941af1
itxnargis <itxnargiskhatun@gmail.com>
2024-12-22 20:08:16 +0530

    feat - Add footer and fix home section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,27 @@
+import { Github, Linkedin, Mail, Twitter } from 'lucide-react'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className='bg-gray-800 py-8'>
+      <div className='container mx-auto px-4'>
+        <div className='flex justify-center space-x-6 mb-4'>
+        <SocialIcon href="https://github.com/itxnargis" icon={Github} />
+        <SocialIcon href="mailto:itxnargiskhatun@gmail.com" icon={Mail} />
+        <SocialIcon href="https://twitter.com/81283nargis" icon={Twitter} />
+        <SocialIcon href="https://www.linkedin.com/in/nargis-khatun-4008ab2a9/" icon={Linkedin} />
+        </div>
+        <p className='text-center text-gray-500'>&copy; Nargis Khatun. All rights reserved.</p>
+</div>
+    </footer>
   )
 }
+
+const SocialIcon = ({ href, icon: Icon, label }) => (
+  <a href={href} className="text-gray-400 hover:text-purple-700 transition-colors duration-300 transform hover:scale-110" target="_blank" rel="noopener noreferrer">
+    <span className='sr-only'>{label}</span>
+    <Icon className='h-6 w-6' aria-hidden='true' />
+  </a>
+)
 
 export default Footer
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div className='min-h-screen flex flex-col justify-center items-center text-center px-4'>
-      <h1 className='text-5xl md:text-7xl font-bold mb-4 bg-gradient-to-r from-purple-400 to-pink-600 text-transparent bg-clip-text tranform transition-all duration-500 ease-in-out hover:scale-105'>Hi, I'm Nargis</h1>
+      <h1 className='text-5xl leading-normal md:text-7xl font-bold mb-4 !leading-normal bg-gradient-to-r from-purple-400 to-pink-600 text-transparent bg-clip-text tranform transition-all duration-500 ease-in-out hover:scale-105'>Hi, I'm Nargis</h1>
       <h2 className='text-3xl md:text-4xl mb-8 tranform transition-all duration-500 ease-in-out hover:scale-105'>Web Developer</h2>
       <p className='text-xl mb-12 max-w-2xl tranform transition-all duration-500 ease-in-out hover:scale-105'>Passionate about creating beautiful and user-friendly web experiences.</p>
 
